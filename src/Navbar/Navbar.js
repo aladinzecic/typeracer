c61ba45a95886cced0a09564eb84bfde549fe14e
aladinzecic <aladinzecic10@gmail.com>
2023-12-31 22:10:10 +0100

    update logo

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import logo from '../logo.png'
-import "./Navbar.css"
+import React from "react";
+import logo from "../logo.png";
+import "./Navbar.css";
 export default function Navbar() {
   return (
-    <div className='nav'>
-      <img src={logo} alt='img'></img>
-      <h2>TYPERACER</h2>
+    <div className="nav">
+      <img src={logo} alt="img"></img>
+      <h2>TYPERACERX</h2>
     </div>
-  )
+  );
 }
